8d16f3ea280b4ed105f45f445b7837866fd80ccd
gamer <openaixander@gmail.com>
2025-07-26 07:41:28 +0100

    Django

--- pastq/settings.py
+++ pastq/settings.py
@@ -144,6 +144,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/media/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'faculty/static/faculty',
+]
 # This production code might break development mode, so we check whether we're in DEBUG mode
 # if not DEBUG:
 #     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
