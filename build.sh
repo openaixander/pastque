c35d331dcfb463e8458e02e78a3b1ce466032340
gamer <openaixander@gmail.com>
2025-07-31 07:59:01 +0100

    This is the final push

--- build.sh
+++ build.sh
@@ -14,7 +14,6 @@ python manage.py collectstatic --noinput
 
 # Apply any outstanding database migrations
 python manage.py makemigrations
-python manage.py migrate accounts zero
 
 python manage.py migrate && python manage.py createsuperuser_if_none_exists
 
